9c529cf172ccd9f8b6ab6b5af50c256606a3be3f
Evan Jamaq <xevan19@gmail.com>
2024-11-26 14:56:28 +0700

    update

--- services/db.php
+++ services/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "ayobelajar");
+$conn = new mysqli("sql205.infinityfree.com", "if0_37789147", "Evan190604", "if0_37789147_ayobelajar");
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
